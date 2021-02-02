149a58ddd6eb0c14fbe237e3e42847af99f64a4a
Mark Skilbeck <markskilbeck@gmail.com>
2021-02-02 14:44:58 +0000

    Load foreign libraries at run-time
    This allows the library-loading system to not depend solely on the
    library name that was used at compile time. Example: If libffi.so.6
    was used at compile time but is not present at run-time, then look for
    alternatives rather than just failing.

--- build-app.lisp
+++ build-app.lisp
@@ -16,28 +16,51 @@
       (output-file (make-pathname :name "quilc"
                                   :type #+win32 "exe" #-win32 nil))
       (system-table (make-hash-table :test 'equal))
-      (entry-point "quilc::entry-point"))
-  (flet ((option-present-p (name)
-           (find name sb-ext:*posix-argv* :test 'string=))
-         (make-toplevel-function (entry)
-           (lambda ()
-             (with-simple-restart (abort "Abort")
-               (funcall (read-from-string entry)
-                        sb-ext:*posix-argv*))))
-         (load-systems-table ()
-           (unless (probe-file "system-index.txt")
-             (error "Generate system-index.txt with 'make system-index.txt' first."))
-           (setf (gethash "quilc" system-table) (merge-pathnames "quilc.asd"))
-           (with-open-file (stream "system-index.txt")
-             (loop
-               :for system-file := (read-line stream nil)
-               :while system-file
-               :do (setf (gethash (pathname-name system-file) system-table)
-                         (merge-pathnames system-file)))))
-         (local-system-search (name)
-           (values (gethash name system-table)))
-         (strip-version-githash (version)
-           (subseq version 0 (position #\- version :test #'eql))))
+      (entry-point "quilc::entry-point")
+      (foreign-libraries nil))
+  (labels ((unload-libraries ()
+             ;; A dumped lisp image will use the *exact* foreign library name
+             ;; that was provided at the time the image was dumped. If the
+             ;; library is moved/renamed, CFFI will not try to load an
+             ;; alternative, thus we unload all foreign libraries and tell CFFI
+             ;; to load them at run-time allowing it to pick up alternatives.
+             ;;
+             ;; For a concrete example: when we build the SDK packages for
+             ;; debian, the FFI library available is typically libffi.so.6. On
+             ;; newer versions of debian the library is named libffi.so.7, so
+             ;; when we try to load the lisp image on a newer debian version, it
+             ;; fails with an error about not finding libffi.so.6.
+             (setf foreign-libraries
+                   (mapcar (lambda (library) (funcall (read-from-string "cffi:foreign-library-name") library))
+                           (funcall (read-from-string "cffi:list-foreign-libraries") :loaded-only t)))
+             (map nil (read-from-string "cffi:close-foreign-library") foreign-libraries))
+           (load-libraries ()
+             (pushnew #P"/usr/local/lib/rigetti/" (symbol-value (read-from-string "cffi:*foreign-library-directories*"))
+                      :test #'equal)
+             (map nil (read-from-string "cffi:load-foreign-library") foreign-libraries)
+             (setf foreign-libraries nil))
+           (option-present-p (name)
+             (find name sb-ext:*posix-argv* :test 'string=))
+           (make-toplevel-function (entry)
+             (lambda ()
+               (load-libraries)
+               (with-simple-restart (abort "Abort")
+                 (funcall (read-from-string entry)
+                          sb-ext:*posix-argv*))))
+           (load-systems-table ()
+             (unless (probe-file "system-index.txt")
+               (error "Generate system-index.txt with 'make system-index.txt' first."))
+             (setf (gethash "quilc" system-table) (merge-pathnames "quilc.asd"))
+             (with-open-file (stream "system-index.txt")
+               (loop
+                 :for system-file := (read-line stream nil)
+                 :while system-file
+                 :do (setf (gethash (pathname-name system-file) system-table)
+                           (merge-pathnames system-file)))))
+           (local-system-search (name)
+             (values (gethash name system-table)))
+           (strip-version-githash (version)
+             (subseq version 0 (position #\- version :test #'eql))))
     (load-systems-table)
     (push #'local-system-search asdf:*system-definition-search-functions*)
 
@@ -57,9 +80,10 @@
       (format t "~&Using unsafe entry point~%")
       (setf entry-point "quilc::%entry-point"))
     (force-output)
+    (unload-libraries)
     (sb-ext:save-lisp-and-die output-file
                               :compression #+sb-core-compression t
-                                           #-sb-core-compression nil
+                              #-sb-core-compression nil
                               :save-runtime-options t
                               :executable t
                               :toplevel (make-toplevel-function entry-point))))
